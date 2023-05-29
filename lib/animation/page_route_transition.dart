df042de7ef9481f9f85052e73e9ebf166c6073f6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-29 18:14:34 +0100

    Update page_route_transition.dart

--- lib/animation/page_route_transition.dart
+++ lib/animation/page_route_transition.dart
@@ -5,6 +5,22 @@ class Route1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(_createRoute());
+            },
+            child: const Text('Go!')),
+      ),
+    );
   }
-}
+  
+ Route _createRoute() {
+  return PageRouteBuilder(pageBuilder: ((context, animation, secondaryAnimation) => const Route2(), 
+  transitionsB))
+
+ }
+
+
+
