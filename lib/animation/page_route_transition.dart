088a1b6cf9e120506048a5a3cc648d67719fee6a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-29 18:09:12 +0100

    Create Page Route

---
+++ lib/animation/page_route_transition.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Route1 extends StatelessWidget {
+  const Route1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
