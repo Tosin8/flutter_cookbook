045623d94b9b7a861a725143872a01a3d1d98792
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-30 20:10:58 +0100

    Update page_route_transition.dart

--- lib/animation/page_route_transition.dart
+++ lib/animation/page_route_transition.dart
@@ -22,7 +22,22 @@ Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => const Route2(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      // creating a tween, to animate in from the bottom: animating from Offset 0,1 to Offset 0,0.
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      // using a curveTween
+      const curve = Curves.ease;
+      //var curveTween = CurveTween(curve: curve);
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      // final offsetAnimation = animation.drive(tween);
+
+      // combining the tween using chains.
+
+      // using an animatedwidget
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
     },
   );
 }
@@ -32,6 +47,11 @@ class Route2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
+      ),
+    );
   }
 }
