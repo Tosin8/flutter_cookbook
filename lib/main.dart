7bc497dc6cbf80eadb832a5aa2350ff20fd454c9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-30 20:53:34 +0100

    Create fade/in

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cookbook/animation/page_route_transition.dart';
+
+import 'package:flutter_cookbook/animation/physics_simulation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Route1(),
+      home: const PhysicsCardDragDemo(),
     );
   }
 }
