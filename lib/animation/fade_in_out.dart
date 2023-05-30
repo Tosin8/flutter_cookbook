494aae600566f229a1fe1c9665b726f1c1882c62
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-30 21:02:04 +0100

    Update

--- lib/animation/fade_in_out.dart
+++ lib/animation/fade_in_out.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class ObjectFade extends StatefulWidget {
+  const ObjectFade({super.key});
+
+  @override
+  State<ObjectFade> createState() => _ObjectFadeState();
+}
+
+class _ObjectFadeState extends State<ObjectFade> {
+  bool _visible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: const Duration(seconds: 2),
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toggle Opacity',
+        child: const Icon(Icons.flip),
+      ),
+    );
+  }
+}
