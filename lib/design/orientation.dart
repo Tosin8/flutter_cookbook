f517161c64dd13ea5ae1c1665c284267da48d2af
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-05-31 16:33:01 +0100

    Create tabs

--- lib/design/orientation.dart
+++ lib/design/orientation.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class Orientation_Grid extends StatelessWidget {
+  const Orientation_Grid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'GridView Builder',
+        ),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: OrientationBuilder(builder: (context, orientation) {
+        return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: List.generate(100, (index) {
+              return Center(
+                  child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.displayLarge,
+              ));
+            }));
+      }),
+    );
+  }
+}
