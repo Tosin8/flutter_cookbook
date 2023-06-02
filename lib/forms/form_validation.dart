231760e0b00facd37919c7b0d6b21155b23bc918
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-02 22:14:47 +0100

    Create form_validation

---
+++ lib/forms/form_validation.dart
@@ -0,0 +1,20 @@
+// Steps:
+// 1. Create a form with a global key, important to create a global key when validating a form.
+// 2. Add textform field with validtion logic
+// 3. Create a button to validate and submit the form.
+
+import 'package:flutter/material.dart';
+
+class Form_Valid extends StatefulWidget {
+  const Form_Valid({super.key});
+
+  @override
+  State<Form_Valid> createState() => _Form_ValidState();
+}
+
+class _Form_ValidState extends State<Form_Valid> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
