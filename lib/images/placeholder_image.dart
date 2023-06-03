7ffd6bbf1944fd9d54edd834ed498beec636e5ca
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-03 22:54:09 +0100

    Updating with Stateful class

--- lib/images/placeholder_image.dart
+++ lib/images/placeholder_image.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Waiting_Image extends StatefulWidget {
+  const Waiting_Image({super.key});
+
+  @override
+  State<Waiting_Image> createState() => _Waiting_ImageState();
+}
+
+class _Waiting_ImageState extends State<Waiting_Image> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
