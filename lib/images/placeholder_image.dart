ac734bb5cbb2c25e51223774fcc9f3c6edac14d1
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-04 00:41:45 +0100

    Update placeholder_image.dart

--- lib/images/placeholder_image.dart
+++ lib/images/placeholder_image.dart
@@ -18,7 +18,8 @@ class _Waiting_ImageState extends State<Waiting_Image> {
           children: [
             FadeInImage.assetNetwork(
                 placeholder: 'assets/images/spinner.gif',
-                image: 'assets/images/hamburger.jpg'),
+                image:
+                    'https://images.unsplash.com/photo-1677463037178-bd8b3a97ceff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'),
           ],
         ));
   }
