a7ea962dece14c8e9e6c0e75a0e3fb465aef7dde
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-03 21:21:05 +0100

    Updating focus txtfield

--- lib/forms/focus_textfield.dart
+++ lib/forms/focus_textfield.dart
@@ -15,11 +15,42 @@ class MyCustomForm extends StatefulWidget {
 }
 
 class _MyCustomFormState extends State<MyCustomForm> {
+  late FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: TextFormField(),
-    );
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const TextField(
+                autofocus: true,
+              ),
+              TextField(
+                focusNode: myFocusNode,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => myFocusNode.requestFocus(),
+          tooltip: 'Focus Second Text Field',
+          child: const Icon(Icons.edit),
+        ));
   }
 }
