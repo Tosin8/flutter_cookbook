8777008a7969b62c27aa8fb4995b72eedc4624e8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-04 00:48:06 +0100

    Update placeholder_image.dart

--- lib/images/placeholder_image.dart
+++ lib/images/placeholder_image.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// transparent_image package can still be used for the placeholder image.
+
 class Waiting_Image extends StatefulWidget {
   const Waiting_Image({super.key});
 
@@ -20,6 +22,11 @@ class _Waiting_ImageState extends State<Waiting_Image> {
                 placeholder: 'assets/images/spinner.gif',
                 image:
                     'https://images.unsplash.com/photo-1677463037178-bd8b3a97ceff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'),
+            const SizedBox(height: 5),
+            FadeInImage.assetNetwork(
+                placeholder: 'assets/images/spinner.gif',
+                image:
+                    'https://images.unsplash.com/photo-1524786321032-10cb811ac583?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80'),
           ],
         ));
   }
