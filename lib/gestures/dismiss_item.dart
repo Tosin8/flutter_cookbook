2536ba3b7bc368f02f2bdd9fca4b329faef7eac8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-03 22:22:20 +0100

    Update dismiss_item.dart

--- lib/gestures/dismiss_item.dart
+++ lib/gestures/dismiss_item.dart
@@ -8,14 +8,26 @@ class Item_Swap extends StatefulWidget {
 }
 
 class _Item_SwapState extends State<Item_Swap> {
+  final items = List<String>.generate(20, (i) => 'Item ${i + 1}');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
           itemCount: items.length,
           itemBuilder: (context, index) {
-            final item = items[index]; 
-            return Dismissible(key: Key(item), child: child)
+            final item = items[index];
+            return Dismissible(
+                key: Key(item),
+                onDismissed: (direction) {
+                  setState(() {
+                    items.removeAt(index);
+                  });
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('$item dismissed')));
+                },
+                child: ListTile(
+                  title: Text(item),
+                ));
           }),
     );
   }
