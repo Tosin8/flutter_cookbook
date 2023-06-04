b54ce5326f91158207e6b8adb1459464a7244524
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-04 01:10:41 +0100

    Update horizontal_list.dart

---
+++ lib/lists/horizontal_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class list_horizontal extends StatelessWidget {
+  const list_horizontal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
