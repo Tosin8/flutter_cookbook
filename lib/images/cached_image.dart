4e307b618a22bf46af4ba041ad922b04d16f930a
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-04 01:03:59 +0100

    Update cached_image.dart

--- lib/images/cached_image.dart
+++ lib/images/cached_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Cached_Image extends StatelessWidget {
@@ -16,7 +17,10 @@ class Cached_Image extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: CachedNetworkImage,
+        child: CachedNetworkImage(
+            placeholder: (context, url) => const CircularProgressIndicator(),
+            imageUrl:
+                'https://images.unsplash.com/photo-1524786321032-10cb811ac583?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80'),
       ),
     );
   }
