9a1b03f3324231b7db9048b4bcf655b4fb594e75
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-04 02:11:48 +0100

    Update floating_appbar.dart

--- lib/lists/floating_appbar.dart
+++ lib/lists/floating_appbar.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+class Floating_Bar extends StatefulWidget {
+  const Floating_Bar({super.key});
+
+  @override
+  State<Floating_Bar> createState() => _Floating_BarState();
+}
+
+class _Floating_BarState extends State<Floating_Bar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Floating Bar'),
+      ),
+    );
+  }
+}
