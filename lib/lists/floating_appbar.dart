19b691d852ec0333160dd530424d8619c35e935a
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-04 02:39:30 +0100

    Updating SliverAppBar

--- lib/lists/floating_appbar.dart
+++ lib/lists/floating_appbar.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Doing the following to create custom scroll list
+//1. Create CustomScrollView
+//2. Using SliverAppBar to add a floating app bar
+// 3. Ading a list of items using a SliverList
+
 class Floating_Bar extends StatefulWidget {
   const Floating_Bar({super.key});
 
@@ -15,7 +20,31 @@ class _Floating_BarState extends State<Floating_Bar> {
         title: const Text('Floating Bar'),
       ),
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          SliverAppBar(
+            title: const Text(
+              'Scroll App',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                'https://images.unsplash.com/photo-1685728399491-87603c952191?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+                fit: BoxFit.cover,
+              ),
+              stretchModes: const [StretchMode.zoomBackground],
+            ),
+            expandedHeight: 200,
+            backgroundColor: Colors.green,
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            (context, index) => ListTile(title: Text('Item #$index')),
+            // Builds 1000 ListTiles
+            childCount: 1000,
+          )),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cookbook/gestures/inkwell_button.dart';
 import 'package:flutter_cookbook/images/cached_image.dart';
 import 'package:flutter_cookbook/images/placeholder_image.dart';
+import 'package:flutter_cookbook/lists/floating_appbar.dart';
 import 'package:flutter_cookbook/lists/horizontal_list.dart';
 import 'package:flutter_cookbook/lists/using_list.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Basic_List(),
+      home: const Floating_Bar(),
     );
   }
 }
