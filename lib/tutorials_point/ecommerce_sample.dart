2add558d568276d337e9836cfe1448d8eb0d448b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-09 22:53:20 +0100

    Update ecommerce_sample.dart

--- lib/tutorials_point/ecommerce_sample.dart
+++ lib/tutorials_point/ecommerce_sample.dart
@@ -10,6 +10,18 @@ class eCommerce extends StatefulWidget {
 class _eCommerceState extends State<eCommerce> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Shoe Catalog',
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          backgroundColor: Colors.yellow,
+        ),
+        body: ListView(children: [
+          Row(
+            children: [Image.asset(''), Expanded(child: Column())],
+          ),
+        ]));
   }
 }
